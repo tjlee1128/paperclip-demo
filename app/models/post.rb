d0898191c5c8a65214b170793bca44a7c5c49bf5
tjlee@woowahan.com <tjlee@woowahan.com>
2016-01-19 22:59:23 +0900

    first commit

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ApplicationRecord
+  has_attached_file :image, style: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+end
